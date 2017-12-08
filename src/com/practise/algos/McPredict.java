079c0db5f680a6d3db4e01bd53fee6e5632b3076
naveenkumarmarri <naveenkumarmarri6014@gmail.com>
2017-12-07 19:56:58 -0500

    test

--- src/com/practise/algos/McPredict.java
+++ src/com/practise/algos/McPredict.java
@@ -5,7 +5,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Mckinsey {
+public class McPredict {
 
 	public static void main(String[] args) throws ParseException {
 		
