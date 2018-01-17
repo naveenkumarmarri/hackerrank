0c32c5a04f947003c7a2b274819113b1c0c745cb
naveenkumarmarri <naveenkumarmarri6014@gmail.com>
2018-01-17 13:44:50 -0500

    Hacker rank solutions

--- src/LeastCommonAncestor.java
+++ src/LeastCommonAncestor.java
@@ -1,40 +1,48 @@
 import java.util.Stack;
 
-class Node_1 {
+class Node_2 {
     int data;
-    Node_1 left;
-    Node_1 right;
+    Node_2 left;
+    Node_2 right;
 }
 	
 public class LeastCommonAncestor {
 
 
+
 	 /* Node is defined as :
-	 
+	 class Node 
+	    int data;
+	    Node left;
+	    Node right;
 	    
 	    */
 
 	static Stack<Integer> stack = new Stack<Integer>();
 	static Stack<Integer> stack1 = new Stack<Integer>();
 
-	static Node_1 lca(Node_1 root,int v1,int v2)
+	static Node_2 lca(Node_2 root,int v1,int v2)
 	    {
+	        Node_2 temp = root;
 	        find(root, v1);
-	        find_1(root, v2);
+	        find_1(temp, v2);
 	        int v_1 = stack.size();
 	        int v_2 = stack1.size();
 	        if(v_1 < v_2){
-	            while(v_1 != v2){ 
+	            while(v_1 != v_2){ 
 	                stack1.pop();
+	                v_2--;
 	            }
 	        }
 	        else if(v_1 > v_2){
 	            while(v_1 != v2){ 
 	                stack.pop();
+	                v_1--;
 	            }
 	        }
+	        
 	        int lca = -1;
-	        for(int i=0;i<stack.size();i++) {
+	        while(stack.size()!=0 && stack1.size()!=0) {
 	            int temp_1 = stack.pop();
 	            int temp_2 = stack1.pop();
 	            boolean found = false;
@@ -47,10 +55,11 @@ public class LeastCommonAncestor {
 	                break;
 	                
 	        }
+	    System.out.println(lca);
 	        return search(root, lca);
 	    }
 
-	static Node_1 search(Node_1 root, int l) {
+	static Node_2 search(Node_2 root, int l) {
 	    
 	    if(root.data == l)
 	        return root;
@@ -64,9 +73,9 @@ public class LeastCommonAncestor {
 	            search(root.right, l);
 	        }
 	    }
-	    return;
+	    return null;
 	}
-	static void find(Node_1 root, int v) {
+	static void find(Node_2 root, int v) {
 	    if(root == null)
 	        return;
 	    if(root.data == v) {
@@ -86,7 +95,7 @@ public class LeastCommonAncestor {
 
 	}
 
-	static void find_1(Node_1 root, int v) {
+	static void find_1(Node_2 root, int v) {
 	    if(root == null)
 	        return;
 	    if(root.data == v) {
@@ -96,14 +105,16 @@ public class LeastCommonAncestor {
 	    if(root.data < v) {
 	        stack1.push(root.data);
 	        if(root.left != null)
-	              find(root.left, v);
+	              find_1(root.left, v);
 	    }
 	    if(root.data > v) {
 	        stack1.push(root.data);
 	        if(root.right != null)
-	            find(root.right, v);
+	            find_1(root.right, v);
 	    }
 	}
+
+
 
 
 
