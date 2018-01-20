73b8c6b2fa4b1aed155215cf4a6a4552de0b3a1c
naveenkumarmarri <naveenkumarmarri6014@gmail.com>
2018-01-19 22:13:43 -0500

    Longest common subsequence

---
+++ src/com/practise/algos/LongestCommongSubSeq.java
@@ -0,0 +1,34 @@
+package com.practise.algos;
+
+import java.util.Scanner;
+
+public class LongestCommongSubSeq {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int k=0;k<n;k++) {
+			int a_s = sc.nextInt();
+			int b_s = sc.nextInt();
+			String a = sc.next();
+			String b = sc.next();
+			char [] a_ = a.toCharArray();
+			char [] b_ = b.toCharArray();
+			int[][] lcq = new int[a.length()+1][b.length()+1];
+			
+			for(int i=1;i<=a.length();i++) {
+				for(int j=1;j<=b.length();j++) {
+					if(a_[i-1] == b_[j-1]) 
+						lcq[i][j] = 1+lcq[i-1][j-1];
+					else 
+						lcq[i][j] = Math.max(lcq[i-1][j], lcq[i][j-1]);
+					
+				}
+			}
+			System.out.println(lcq[a_.length][b_.length]);
+		}
+		
+		
+	}
+
+}
