a41f049f22753f542b780d02fe15848737064f00
naveenkumarmarri <naveenkumarmarri6014@gmail.com>
2018-01-22 17:16:08 -0500

    Quick sort

--- src/com/practise/algos/QuickSort.java
+++ src/com/practise/algos/QuickSort.java
@@ -1,22 +1,45 @@
 package com.practise.algos;
 
+
 public class QuickSort {
 
 	public static void main(String[] args) {
 			
 		int [] a = {3,6,2,1,9,7};
-		for(int i =0;i<a.length;i++)
-		{
-			int p = partition(a,0,a.length-1);
-			partition(a,0, p);
-			partition(a,p+1,a.length-1);
-		}
+		
+		quickSort(a, 0, a.length-1);
+		for(int i:a)
+			System.out.println(i);
 
 	}
 
-	private static int partition(int[] a, int i, int j) {
+	private static void quickSort(int[] a, int low, int high) {
+		if(low < high) {
+			int pivot = partition(a, low, high);
+			quickSort(a, low, pivot - 1);
+			quickSort(a, pivot+1, high);
+		}
 		
-		return 0;
+	}
+
+	private static int partition(int[] a, int low, int high) {
+		
+		int temp = low;
+		int pivot = a[low];
+		for(int i=low+1; i<= high; i++) {
+			if(pivot < a[i]) {
+				temp++;
+				int x = a[temp];
+				a[temp] = a[i];
+				a[i] = x; 
+				
+			}
+		}
+		int y = a[temp-1];
+		a[temp-1] = a[pivot];
+		a[pivot] = y;
+		
+		return temp;
 	}
 
 }
